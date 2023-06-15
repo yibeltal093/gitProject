19092939fb51579e137ab0cb7e75e84302186d88
yibeltal093 <72584145+yibeltal093@users.noreply.github.com>
2023-06-14 20:07:28 -0700

    java file edited on github

--- neFile.java
+++ neFile.java
@@ -1,8 +1,5 @@
-public class main{}[D
-public static void main()[DString []args[C[D){}[D
-[A[A[A[C[C[C[C[C[B[C[C[C[C[C[C[C[C[M[C[C[C[C[[C[C[C[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[
-}[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-
-}[	System.out.prinln()[D"Hello World");
-
+public class Main{
+  public static void main(String []args)
+    System.out.prinln()[D"Hello World");
+  }
 }
